179db90885ee96a1e520c560e13bf486f5a36277
Rajat Ankel <69978878+rajat99260@users.noreply.github.com>
2023-01-31 15:23:20 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+#Config file
+//Site Title
+$title = "Moko IPTV Player - IPTV Video Streaming Website";
+
+//Site URL
+$site_url = "https://mokoweb.xyz/mokoiptv/";
+
+//Site Description
+$description = "";
+
+//Watermark image
+$watermarkImage = "";
+
+//Watermark link
+$watermarkLink = "";
+
+//Google Analytics tag ID
+$gaCode = "UA-69771681-1";
+
+//Preroll Ads Video (.mp4)
+$prerollAds = "";
+
+//Preroll Ads Time (seconds)
+$prerollTime = 0;
+
+//Preroll Ads External Link
+$prerollLink = "";
+?>
